e2f582e1a53a4e01e8241b8a0d3eb679fb0c656b
KyzutoGH <iksancjr@gmail.com>
2025-05-17 16:27:14 +0700

    Copyright tambah

--- bagian/Copyright.php
+++ bagian/Copyright.php
@@ -1,10 +1,11 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2025 <a href="#">Fukubi Store</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0
-    </div>
-  </footer>
+<!-- Main Footer -->
+<footer class="main-footer">
+  <strong>Copyright &copy; 2025 <a href="#">Fukubi Store</a>.</strong>
+  All rights reserved. <br>
+  <small>Designed by <a href="https://adminlte.io/" target="_blank">AdminLTE 3</a> | Developed by Irregular Camp</small>
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.2.1
   </div>
-  <!-- ./wrapper -->
+</footer>
+</div>
+<!-- ./wrapper -->
