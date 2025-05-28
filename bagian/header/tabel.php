141ca1908938b59d3364357a74472c6e2c9fde4d
KyzutoGH <iksancjr@gmail.com>
2025-05-28 14:58:29 +0700

    Fix header tabel dies natalis

--- bagian/header/tabel.php
+++ bagian/header/tabel.php
@@ -3,12 +3,17 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Tabel <?php echo $menu; ?></h1>
+        <h1> Tabel
+          <?php
+          echo ($menu == "Tabel") ? "Dies Natalis" : $menu;
+          ?>
+        </h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">Tabel <?php echo $menu; ?></li>
+          <li class="breadcrumb-item active">Tabel <?php echo ($menu == "Tabel") ? "Dies Natalis" : $menu;
+          ?></li>
         </ol>
       </div>
     </div>
